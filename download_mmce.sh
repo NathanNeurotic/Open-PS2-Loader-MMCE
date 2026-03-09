9e14745a4633ab840d7cb96d209cc18e6a40c400
NathanNeurotic <noahtkevans@gmail.com>
2026-03-09 12:29:25 -0700

    Investigate lwNBD download failure

--- download_mmce.sh
+++ download_mmce.sh
@@ -10,7 +10,18 @@ mkdir -p "$REPO_FOLDER"
 
 for file in "${FILES[@]}"; do
   if test ! -f "$REPO_FOLDER/$file"; then
-    curl -L --fail --silent --show-error "$REPO_URL/$file" -o "$REPO_FOLDER/$file.tmp" || { exit 1; }
+    python3 - "$REPO_URL/$file" "$REPO_FOLDER/$file.tmp" <<'PY'
+import pathlib
+import shutil
+import sys
+import urllib.request
+
+url = sys.argv[1]
+output = pathlib.Path(sys.argv[2])
+
+with urllib.request.urlopen(url) as response, output.open("wb") as destination:
+    shutil.copyfileobj(response, destination)
+PY
     mv "$REPO_FOLDER/$file.tmp" "$REPO_FOLDER/$file"
   fi
 done
