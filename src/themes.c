e496ddf38c2510f96144e6f97b83981ad2fe485e
NathanNeurotic <noahtkevans@gmail.com>
2026-03-16 14:20:25 -0700

    Review MMCE workflow updates

--- src/themes.c
+++ src/themes.c
@@ -1180,7 +1180,8 @@ static void clampSelectedCoverCaches(theme_t *theme, theme_elems_t *elems)
             if (gameImage != NULL && gameImage->cache != NULL && gameImage->cache->suffix != NULL && strcmp(gameImage->cache->suffix, "COV") == 0 &&
                 !isDecoratorCoverCache(theme->gamesItemsList, gameImage->cache) && !isDecoratorCoverCache(theme->appsItemsList, gameImage->cache)) {
                 gameImage->cache->allowPrime = 0;
-                gameImage->cache->count = 2;
+                if (gameImage->cache->count < 2)
+                    gameImage->cache->count = 2;
             }
         }
 
