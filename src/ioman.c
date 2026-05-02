307f45807ae497584bfa2748f7df4bb213d338dc
NathanNeurotic (Ripto) <noahtkevans@gmail.com>
2026-05-02 01:22:07 -0700

    ioman: yield while waiting for pending I/O during block

--- src/ioman.c
+++ src/ioman.c
@@ -362,6 +362,7 @@ int ioBlockOps(int block)
 
         // wait for all io to finish
         while (ioHasPendingRequests()) {
+            DelayThread(1000);
         };
 
         ChangeThreadPriority(ThreadID, status.current_priority);
