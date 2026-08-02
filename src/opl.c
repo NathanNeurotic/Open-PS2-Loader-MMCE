b0639a7ad19df2f508ceb5d619f32fc2b1232984
NathanNeurotic (Ripto) <contact@nathanneurotic.com>
2026-08-02 13:52:22 -0700

    Merge pull request #347 from NathanNeurotic/fix/307-nbd-modal-underlay
    fix(gui): fix NBD server modal background rendering (#307)
    
    (cherry picked from commit 394184a45e7033964b51f1dd8e973b8f172b4726)

--- src/opl.c
+++ src/opl.c
@@ -2723,9 +2723,9 @@ void handleLwnbdSrv()
     guiRenderTextScreen(_l(_STR_STARTINGNBD));
     if (loadLwnbdSvr() == 0) {
         snprintf(temp, sizeof(temp), "%s", _l(_STR_RUNNINGNBD));
-        guiMsgBox(temp, 0, diaToolsConfig);
+        guiMsgBox(temp, 0, NULL);
     } else
-        guiMsgBox(_l(_STR_STARTFAILNBD), 0, diaToolsConfig);
+        guiMsgBox(_l(_STR_STARTFAILNBD), 0, NULL);
 
     // restore normal functionality again
     guiRenderTextScreen(_l(_STR_UNLOADNBD));
